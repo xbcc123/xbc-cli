0ac91e62238daac105747e4439265594ef80b488
ykj <287207951@qq.com>
2019-07-16 22:25:29 +0800

    [feat]: 新增文件

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-// var fs = require('fs');
-// var fse = require('fs-extra');
-// var path = require('path');
+var fs = require('fs');
+var fse = require('fs-extra');
+var path = require('path');
 
-// var PATH = process.argv[2] ? './' + process.argv[2] : './client';
+var PATH = process.argv[2] ? './' + process.argv[2] : './client';
 
-console.log(process.argv)
+console.log(PATH)
 
-
-// fs.mkdir(PATH, function (err) {
-//     err && console.log(err);
-// });
-// fse.copy(path.join(__dirname, 'templates'), PATH);
+fs.mkdir(PATH, function (err) {
+    err && console.log(err);
+});
+fse.copy(path.join(__dirname, 'package'), PATH);
